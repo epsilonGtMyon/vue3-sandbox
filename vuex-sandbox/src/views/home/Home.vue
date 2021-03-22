80017565ceedb1d0e7860ec6ccb52ef2d10a842e
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-03-22 22:28:08 +0900

    add new project vuex4-sandbox

---
+++ vuex-sandbox/src/views/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <ul>
+      <li>
+        <router-link to="/sandbox01">Sandbox01</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  setup() {
+    return {};
+  },
+});
+</script>
